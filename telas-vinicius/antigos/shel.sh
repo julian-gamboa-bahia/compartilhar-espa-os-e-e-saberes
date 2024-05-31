29716758c41f6b6fc5ccf2e34cf46d66336b4c04
Your Name <you@example.com>
2024-05-31 04:22:55 -0300

    poema https://www.in.gov.br/leiturajornal

---
+++ telas-vinicius/antigos/shel.sh
@@ -0,0 +1,16 @@
+
+
+
+wget https://crpme.onrender.com/agendamento.html
+
+
+wget https://crpme.onrender.com/index.html
+wget https://crpme.onrender.com/login.html
+wget https://crpme.onrender.com/cadastro_salas.html
+
+exit
+
+
+wget 
+wget 
+wget 
